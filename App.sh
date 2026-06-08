dcbaa25a7120573ce41ad849e734dc7064b5697c
madi <mz2284326@gmail.com>
2026-06-08 10:27:44 +0330

    Edit app.sh

--- App.sh
+++ App.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+read -p "Enter hash: " hash
+param=$(echo $hash | awk -F "$" '{ print $3}' )
+salt=$(echo $hash | awk -F "$" '{ print $4}' )
+endsalt=$(echo $param'$'$salt)
+for i in $(cat pass)
+do
+generated=$(mkpasswd --method=yescrypt --salt=$endsalt $i)
+if [ $hash == $generated ]
+then
+echo "Password is: $i"
+exit
+fi
+done
